4458e1186f7feb5f8c581480d4b4e482a42c22db
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-07-03 21:33:56 +0200

    feat
    add css to Card, add console.log(cartItems) because i have bug : main.7359e58c3cd5ac54f3ec.hot-update.js:116 Uncaught ReferenceError: cart is not defined
        at Cart (main.7359e58c3cd5ac54f3ec.hot-update.js:116:15)
        at renderWithHooks (react-dom.development.js:16175:1)
        at updateFunctionComponent (react-dom.development.js:20387:1)
        at beginWork (react-dom.development.js:22430:1)
        at HTMLUnknownElement.callCallback (react-dom.development.js:4161:1)
        at Object.invokeGuardedCallbackDev (react-dom.development.js:4210:1)
        at invokeGuardedCallback (react-dom.development.js:4274:1)
        at beginWork$1 (react-dom.development.js:27405:1)
        at performUnitOfWork (react-dom.development.js:26513:1)
        at workLoopSync (react-dom.development.js:26422:1)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { selectProducts } from '../store/productSlice';
 import { addToCart } from '../store/cartSlice';
 import { selectCart } from '../store/cartSlice';
-
+import css from '../components/App.module.css';
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
@@ -15,12 +15,16 @@ const ProductCard = ({ product }) => {
 
   return (
     <Col md={4} className="mb-4">
-      <Card>
+      <Card className={css.card}>
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Text>{product.description}</Card.Text>
           <Card.Text>Price: ${product.price.toFixed(2)}</Card.Text>
-          <Button variant="primary" onClick={handleAddToCart} className="custom-button">
+          <Button
+            variant="primary"
+            onClick={handleAddToCart}
+            className="custom-button"
+          >
             Add to Cart
           </Button>
         </Card.Body>
@@ -31,7 +35,7 @@ const ProductCard = ({ product }) => {
 
 const Cart = () => {
   const cartItems = useSelector(selectCart);
-
+console.log(cartItems)
   return (
     <div>
       <h2 className="my-4">Cart</h2>
@@ -40,7 +44,7 @@ const Cart = () => {
       ) : (
         <ul>
           {cartItems.map(item => (
-            <li key={item.id}>{item.name}</li>
+            <li key={item.cartItemId}>{item.name}</li>
           ))}
         </ul>
       )}
@@ -48,7 +52,7 @@ const Cart = () => {
   );
 };
 
-export const App = () => {
+const App = () => {
   const products = useSelector(selectProducts);
 
   return (
